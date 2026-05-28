4204dc659b09f2586f1da5fb57b34b552695a303
Aengus MacKenzie <aengus.2000@gmail.com>
2026-05-28 11:49:25 -0400

    Used hash table to validate ransomNote construction

--- ransom_note.py
+++ ransom_note.py
@@ -10,4 +10,18 @@ def can_construct(ransomNote: str, magazine: str) -> bool:
     Returns:
         bool: True if ransomNote can be constructed, False otherwise.
     """
-    pass  # TODO: Implement this function
+    letter_count = {}
+
+    for char in magazine:
+        if char in letter_count:
+            letter_count[char] += 1
+        else:
+            letter_count[char] = 1
+
+    for char in ransomNote:
+        if char not in letter_count or letter_count[char] == 0:
+            return False
+        
+        letter_count[char] -= 1
+        
+    return True
